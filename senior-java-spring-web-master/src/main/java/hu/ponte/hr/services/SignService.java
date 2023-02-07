f5210e46fa96cfe16b208347b86f2d3aeedf2fef
Vancsura <valaci1992@gmail.com>
2023-02-07 16:47:57 +0100

    Sign made by SHA256withRSA algorithm and encoded by BASE64. Result shown on localhost:8080.

--- senior-java-spring-web-master/src/main/java/hu/ponte/hr/services/SignService.java
+++ senior-java-spring-web-master/src/main/java/hu/ponte/hr/services/SignService.java
@@ -3,9 +3,26 @@ package hu.ponte.hr.services;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.Signature;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
 
 @Service
 @Transactional
 public class SignService {
+
+    public String signSHA256RSA(String input, byte[] strPk) throws Exception {
+
+        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(strPk);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+
+        Signature privateSignature = Signature.getInstance("SHA256withRSA");
+        privateSignature.initSign(kf.generatePrivate(spec));
+        privateSignature.update(input.getBytes(StandardCharsets.UTF_8));
+        byte[] s = privateSignature.sign();
+        return Base64.getEncoder().encodeToString(s);
+    }
 
 }
